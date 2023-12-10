245c352964ea6df612851873b19322edc4aa05ab
sofiahag <sofia.h4glund@gmail.com>
2023-12-10 16:56:05 +0100

    clean up code a bit

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -15,18 +15,52 @@ const api = {
   base: 'https://api.openweathermap.org/data/2.5/',
 };
 
-//TODO Clean up code
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Sofia Pro, Arial, Helvetica, sans-serif',
+    textAlign: 'center',
+  },
+  palette: {
+    primary: {
+      main: '#c2410c'
+    }
+  }
+});
+
+const weatherAnimations = {
+  Clear: sun,
+  Dust: mist,
+  Smoke: mist,
+  Clouds: clouds,
+  Rain: rain,
+  Drizzle: rain,
+  Thunderstorm: lightning, 
+  Mist: mist,
+  Haze: mist,
+  Fog: mist,
+  Snow: snow,
+}; 
+
+const initialSky = {
+  Clear: 'linear-gradient(#48a6ee, #48a6ee, #f2d079)',
+  Dust: 'linear-gradient(#8ab5dc, #aeb3b8)',
+  Smoke: 'linear-gradient(#8ab5dc, #aeb3b8)',
+  Clouds: 'linear-gradient(#48a6ee, #48a6ee, #88b8dd, #abcde7)',
+  Rain: 'linear-gradient(#696969, #696969, #b7c1d0)',
+  Drizzle: 'linear-gradient(#696969, #696969, #b7c1d0)',
+  Thunderstorm: 'linear-gradient(#696969, #696969, #b7c1d0)',
+  Mist: 'linear-gradient(#48a6ee, #7eb6d6, #d1dee8)',
+  Haze: 'linear-gradient(#48a6ee, #7eb6d6, #d1dee8)',
+  Fog: 'linear-gradient(#48a6ee, #7eb6d6, #d1dee8)',
+  Snow: 'linear-gradient(#48a6ee, #7eb6d6, #d1dee8)',
+};
+
+//TODO: add styles to CSS
 
 function Weather() {
   const [query, setQuery] = useState('');
   const [weather, setWeather] = useState({});
   const [sky, setSky] = useState('linear-gradient(#799cf2, #b194f5, #f5a793, #f5e994)');
-
-  const clearSky = 'linear-gradient(#48a6ee, #48a6ee, #f2d079)';
-  const dustySky = 'linear-gradient(#8ab5dc, #aeb3b8)';
-  const cloudySky = 'linear-gradient(#48a6ee, #48a6ee, #88b8dd, #abcde7)';
-  const rainySky = 'linear-gradient(#696969, #696969, #b7c1d0)';
-  const mistySky = 'linear-gradient(#48a6ee, #7eb6d6, #d1dee8)';
 
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
@@ -36,27 +70,20 @@ function Weather() {
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
+    minWidth: '100vh', 
+    height: '100vh', 
+    overflow: 'hidden'
   };
 
-  const theme = createTheme({
-    typography: {
-      fontFamily: 'Sofia Pro, Arial, Helvetica, sans-serif',
-      textAlign: 'center',
-    },
-    palette: {
-      primary: {
-        main: '#c2410c'
-      }
-    }
-  });
-
+  const isLargeScreen = windowWidth > 1501;
+  const isLaptopScreen = windowWidth >= 1025 && windowWidth <= 1500;
   const isMediumScreen = windowWidth >= 768 && windowWidth <= 1024;
   const isSmallScreen = windowWidth < 768;
   
   const weatherBoxStyles = {
     position: 'absolute',
     zIndex: '1',
-    top: isMediumScreen ? '48%' : '70%',
+    top: isMediumScreen ? '48%' : (isLargeScreen ? '50%' : '70%'),
     left: '50%',
     transform: 'translate(-50%, -50%)',
   };
@@ -82,20 +109,24 @@ function Weather() {
     const handleResize = () => {
       setWindowWidth(window.innerWidth);
     };
-
     window.addEventListener('resize', handleResize);
-
     return () => {
       window.removeEventListener('resize', handleResize);
     };
   }, []);
+
+  useEffect(() => {
+    if (weather.main && weather.weather) {
+      const condition = weather.weather[0].main;
+      setSky(initialSky[condition] || initialSky.Clear);
+    }
+  }, [weather]);
 
   const search = async () => {
     try {
       const response = await fetch(`${api.base}weather?q=${query}&appid=${api.key}&units=metric`);
       const result = await response.json();
       if (response.ok) {
-        toggleBg(result.weather[0].main);
         setWeather(result);
         setQuery('');
       } else {
@@ -104,34 +135,6 @@ function Weather() {
     } catch (error) {
       console.error('Error fetching weather data:', error);
       setWeather({ error: 'Error fetching weather data' });
-    }
-  };
-
-  const toggleBg = (w_data) => {
-    switch (w_data) {
-      case 'Clear':
-        setSky(clearSky);
-        break;
-      case 'Rain':
-      case 'Drizzle':
-      case 'Thunderstorm':
-        setSky(rainySky);
-        break;
-      case 'Clouds':
-        setSky(cloudySky);
-        break;
-      case 'Mist':
-      case 'Haze':
-      case 'Fog':
-      case 'Snow':
-        setSky(mistySky);
-        break;
-      case 'Dust':
-      case 'Smoke':
-        setSky(dustySky);
-        break;
-      default:
-        setSky(clearSky);
     }
   };
 
@@ -154,7 +157,7 @@ function Weather() {
 
   return (
     <ThemeProvider theme={theme}>
-    <div style={{ ...backgroundStyles, width: '100%', height: '100vh', overflow: 'hidden' }} component="main">
+    <div style={{ ...backgroundStyles }} component="main">
       <Box textAlign="center" mt={8}>
         <Typography variant="h3" style={{ color: 'white', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.3)' }}>
           Current weather
@@ -183,73 +186,42 @@ function Weather() {
         </Box>
       )}
       {weather.main && (
-        <Box>
-          {weather.weather[0].main === 'Clouds' && (
-            <Box style={lottieStyles}>
-              <Lottie animationData={clouds} loop />
-            </Box>
-          )}
-          {weather.weather[0].main === 'Thunderstorm' && (
-            <Box style={lottieStyles}>
-              <Lottie animationData={lightning} loop />
-            </Box>
-          )}
-          {(weather.weather[0].main === 'Drizzle' || weather.weather[0].main === 'Rain') && (
-            <Box style={lottieStyles}>
-              <Lottie animationData={rain} loop />
-            </Box>
-          )}
-          {weather.weather[0].main === 'Clear' && (
-            <Box style={{ marginTop: '330px' }}>
-              <Lottie animationData={sun} loop />
-            </Box>
-          )}
-          {weather.weather[0].main === 'Snow' && (
-            <Box style={lottieStyles}>
-              <Lottie animationData={snow} loop />
-            </Box>
-          )}
-          {(weather.weather[0].main === 'Mist' || weather.weather[0].main === 'Fog'
-            || weather.weather[0].main === 'Dust' || weather.weather[0].main === 'Smoke'
-            || weather.weather[0].main === 'Haze') && (
-            <Box style={{ marginTop: '330px' }}>
-              <Lottie animationData={mist} loop />
-            </Box>
-          )}
-          {weather.main && weather.sys && (
-          <Box style={weatherBoxStyles}>
-            <Box className="location-box" textAlign="center">
-              <Typography className="location" variant="h5" style={{ color: 'white', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.3)' }}>
-                {weather.name}, {weather.sys.country}
+          <Box className="lottie-box">
+            <Lottie animationData={weatherAnimations[weather.weather[0].main]} loop />
+          </Box>
+      )}
+      {weather.main && weather.sys && (
+        <Box style={weatherBoxStyles}>
+          <Box className="location-box" textAlign="center">
+            <Typography className="location" variant="h5" style={{ color: 'white', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.3)' }}>
+              {weather.name}, {weather.sys.country}
+            </Typography>
+            <Typography className="date" variant="subtitle2" style={{ color: 'white', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.3)' }}>
+              {dateBuilder(new Date())}
+            </Typography>
+            <Typography className="text-md" variant="body2" style={{ color: 'white', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.3)' }}>
+              Humidity: {weather.main.humidity}%
+            </Typography>
+            <Typography className="text-md" variant="body2" style={{ color: 'white', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.3)' }}>
+              Wind: {weather.wind.speed} m/s
+            </Typography>
+            <Typography className="text-md" variant="body2" style={{ color: 'white', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.3)' }}>
+              Clouds: {weather.clouds.all}%
+            </Typography>
+            <Typography variant="h5" style={{ color: 'white', fontSize: '1.7rem', marginTop: '2px', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.3)' }}>
+              {weather.weather[0].main}
+            </Typography>
+          </Box>
+          <Box className="weather-box" textAlign="center">
+            <Box className="temp" style={{ display: 'inline-block', position: 'relative', margin: '5px 0 0 0', padding: '20px 20px 10px 20px', backgroundColor: 'rgba(255, 255, 255, 0.507)', borderRadius: '30px', boxShadow: '3px 4px rgba(76, 76, 76, 0.412)' }}>
+              <Typography variant="h1" color="primary">
+                {Math.round(weather.main.temp)}°C
               </Typography>
-              <Typography className="date" variant="subtitle2" style={{ color: 'white', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.3)' }}>
-                {dateBuilder(new Date())}
-              </Typography>
-              <Typography className="text-md" variant="body2" style={{ color: 'white', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.3)' }}>
-                Humidity: {weather.main.humidity}%
-              </Typography>
-              <Typography className="text-md" variant="body2" style={{ color: 'white', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.3)' }}>
-                Wind: {weather.wind.speed} m/s
-              </Typography>
-              <Typography className="text-md" variant="body2" style={{ color: 'white', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.3)' }}>
-                Clouds: {weather.clouds.all}%
-              </Typography>
-              <Typography variant="h5" style={{ color: 'white', fontSize: '1.7rem', marginTop: '2px', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.3)' }}>
-                {weather.weather[0].main}
-              </Typography>
-            </Box>
-            <Box className="weather-box" textAlign="center">
-              <Box className="temp" style={{ display: 'inline-block', position: 'relative', margin: '5px 0 0 0', padding: '20px 20px 10px 20px', backgroundColor: 'rgba(255, 255, 255, 0.507)', borderRadius: '30px', boxShadow: '3px 4px rgba(76, 76, 76, 0.412)' }}>
-                <Typography variant="h1" color="primary">
-                  {Math.round(weather.main.temp)}°C
-                </Typography>
-              </Box>
             </Box>
           </Box>
-          )}
         </Box>
-      )}
-    </div>
+        )}
+      </div>
     </ThemeProvider>
   );
 }
